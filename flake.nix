5628e10b7c6ce2b8829eb8d389dd893eda310f9d
Darragh Elliott <me@delliott.xyz>
2025-03-04 10:46:02 +0000

    feat: add nix support for building etc

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    blueprint.url = "github:numtide/blueprint";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.blueprint {
+      inherit inputs;
+      prefix = "./nix/";
+    };
+}
---
+++ nix/packages/lunar-network-daemon.nix
@@ -0,0 +1,23 @@
+{
+  flake, # maps to inputs.self
+  pkgs, # a nixpkgs instance
+  debug ? false,
+  ...
+}:
+let
+  inherit (pkgs) lib;
+in
+pkgs.stdenv.mkDerivation {
+  name = "all";
+  src = flake;
+  nativeBuildInputs = with pkgs; [
+    cmake
+  ];
+  buildInputs = with pkgs; [
+    nlohmann_json
+  ];
+  preConfigure = lib.optionalString debug ''
+    echo "Enabling debug build"
+    export cmakeBuildType=Debug
+  '';
+}
