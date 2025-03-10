44ee01a5aa860bdd8493cce335add3225ee72d7e
merkelmarrow <marco.a.blackwell@gmail.com>
2025-03-10 14:14:53 +0000

    build: integrate netfilter dependency in CMake configuration
    - modify the lunar-network-daemon target to link to new static netfilter library alongside packet and config
    - add find_library/find_path checks for libnetfilter_queue and libnfnetlink (netfilter dependency)
    - fixed accidental NetfilterManager redefinitions in NetfilterManager.cpp

--- src/netfilter/NetfilterManager.cpp
+++ src/netfilter/NetfilterManager.cpp
@@ -1,19 +1,2 @@
 // src/netfilter/NetfilterManager.cpp
 #include "NetfilterManager.hpp"
-#include <chrono>
-#include <functional> // std::function
-
-class NetfilterManager {
-public:
-  // type definition for packet handling callback.
-  // when a NetfilterManager instance is created, the user will provide a
-  // callback function that matches this signature
-  using PacketHandlerFunc = std::function<void(
-      uint32_t id, const uint8_t *data, size_t length, uint32_t mark,
-      std::chrono::steady_clock::time_point time_received)>;
-
-  NetfilterManager(const std::string &interface_name, PacketHandlerFunc handler,
-                   int queue_num = 0);
-
-  ~NetfilterManager();
-};
