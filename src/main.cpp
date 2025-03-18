a95a659ad76f3e2418d2e0d9ea8a9459b4d0067b
merkelmarrow <marco.a.blackwell@gmail.com>
2025-03-18 19:20:21 +0000

    feat: implement function in main to set up iptables rules
    Also, removing Netfilter class for now. I'm starting to think that the extra layer of abstraction is making things more complicated than necessary. May put it back at some stage, but for now I'm going to try implement the netfilter interactions directly in main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
+#include <stdexcept>
 
 #include "ConfigManager.hpp"
 #include "Packet.hpp"
 
-int main()
-{
-    // Just testing everything links properly
-    ConfigManager manager("config/config.json");
-    std::cout << manager.getConfig().earth_to_moon.base_bit_error_rate << std::endl;
+int main() {
+  // Just testing everything links properly
+  ConfigManager manager("config/config.json");
+  std::cout << manager.getConfig().earth_to_moon.base_bit_error_rate
+            << std::endl;
+}
+
+// Function to set up iptables rules
+void setupIPTables() {
+  int return_val = 0;
+
+  std::cout << "Setting up iptable rules\n";
+
+  // Forward wireguard traffic to nfqueue
+  // -A FORWARD: Append a rule to the FORWARD chain (ie. packets being routed
+  // through this host) -i wg0: Match packets whose incoming (-i meaning
+  // incoming) interface is wg0 -j NFQUEUE: "Jump" to the NFQUEUE target (ie.
+  // hand off to NFQUEUE instead of dropping or accepting)
+  // --queue-num 0: Put packets into queue number 0.
+  return_val = system("iptables -A FORWARD -i wg0 -j NFQUEUE --queue-num 0");
+  if (return_val != 0) {
+    throw std::runtime_error(
+        "Failed to set up forward iptables rule for incoming traffic.");
+  }
+
+  // Catch outgoing traffic from wg0
+  return_val = system("iptables -A FORWARD -o wg0 -j NFQUEUE --queue-num 0");
+  if (return_val != 0) {
+    // Clean up the previous rule if this one fails
+    system("iptables -D FORWARD -i wg0 -j NFQUEUE --queue-num 0");
+    throw std::runtime_error(
+        "Failed to set up forward iptables rule for outgoing traffic.");
+  }
+
+  std::cout << "iptables rules set up successfully\n";
 }
